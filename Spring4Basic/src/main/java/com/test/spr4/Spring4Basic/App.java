d7161f8901aae74d6a443996c77b840f30079d04
projectabhi <projectabhi@gmail.com>
2016-03-20 13:50:21 +0530

    Initial commit

---
+++ Spring4Basic/src/main/java/com/test/spr4/Spring4Basic/App.java
@@ -0,0 +1,13 @@
+package com.test.spr4.Spring4Basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
