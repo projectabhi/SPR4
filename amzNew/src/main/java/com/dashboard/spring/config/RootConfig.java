47475de7cf935d8b11b901a1beb5562efef63fb8
borokali <borokali@borokali.inter.borokali.com>
2018-10-04 21:25:33 +0530

    Amazon Demo App initial commit

---
+++ amzNew/src/main/java/com/dashboard/spring/config/RootConfig.java
@@ -0,0 +1,9 @@
+package com.dashboard.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+	//Service and Repository beans configuration
+}
